7a066759aca2f385b33b0b6148850fa829350c30
WaleedAkrabi <waleed.akrabi@mail.utoronto.ca>
2024-03-29 18:55:43 -0400

    implemented more of them

--- preventions.py
+++ preventions.py
@@ -63,3 +63,47 @@ def mask_wearing(people, mask_effectiveness):
     for person in people:
         if person.infected and np.random.rand() > mask_effectiveness:
             person.infected = False  # Reduce infection probability based on mask effectiveness
+
+
+def contact_tracing(people, infected_threshold):
+    """
+    Implement contact tracing to identify and isolate individuals who have been in contact with infected individuals.
+    :param people: List of Person objects.
+    :param infected_threshold: Threshold for identifying infected individuals (0 to 1).
+    """
+    for person in people:
+        if person.infected:
+            for contact in person.contacts:
+                if not contact.infected and np.random.rand() < infected_threshold:
+                    contact.infected = True  # Infect contact based on threshold
+                    # Implement further actions such as isolation or testing
+
+
+def ventilation(people, ventilation_effectiveness):
+    """
+    Improve ventilation to reduce the concentration of infectious aerosols in indoor environments.
+    :param people: List of Person objects.
+    :param ventilation_effectiveness: Effectiveness of ventilation (0 to 1).
+    """
+    # Implement ventilation measures such as increasing airflow or using air purifiers
+    pass
+
+
+def staggered_work_hours(people, staggered_factor):
+    """
+    Implement staggered work hours to reduce the number of people present in a shared space at any given time.
+    :param people: List of Person objects.
+    :param staggered_factor: Factor to adjust work hours (0 to 1).
+    """
+    # Adjust work hours for individuals to stagger their arrival and departure times
+    pass
+
+
+def remote_work(people, remote_work_factor):
+    """
+    Encourage remote work to minimize physical interactions in workplaces.
+    :param people: List of Person objects.
+    :param remote_work_factor: Factor to increase remote work (0 to 1).
+    """
+    # Transition individuals to remote work where feasible
+    pass
